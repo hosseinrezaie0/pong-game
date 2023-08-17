5e93d44c36656084a01431f78a4f9b245251bf38
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-08-17 14:02:52 +0330

    black board created

---
+++ pong.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen 
+import time 
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong Game")
+screen.tracer(0)
+screen.listen()
+
+
+
+
+
+
+
+screen.exitonclick()
