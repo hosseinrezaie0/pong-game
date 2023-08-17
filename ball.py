11a9f812021477ec6f066e35ba9a28974c3c0477
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-08-17 14:07:15 +0330

    ball craeted

---
+++ ball.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super.__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+    
+
+    
+        
