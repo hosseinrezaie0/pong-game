e89448d3cc6f5f9f6e53ff139cc23c615c764362
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-08-24 19:27:33 +0330

    while loop added

--- pong.py
+++ pong.py
@@ -13,7 +13,45 @@ screen.tracer(0)
 screen.listen()
 
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+screen.update()
 
+
+
+
+
+
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 285 or ball.ycor() < -285:
+        ball.bounce_y()
+    
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect right paddle misses
+    if ball.xcor() > 380: 
+        ball.reset_position()
+        scoreboard.l_point()
+    
+    # Detect left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 
 
