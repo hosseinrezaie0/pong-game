278f0bdfd0cf759802eebe75ce2f92e6327f5691
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-08-24 13:00:55 +0330

    methods created

--- scoreboard.py
+++ scoreboard.py
@@ -10,3 +10,20 @@ class Scoreboard(Turtle):
         self.l_score = 0
         self.r_score = 0 
         self.update_scoreboard()
+
+    
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align = "center", font = ("classic", 80))
+        self.goto(100, 200)
+        self.write(self.r_score, align = "center", font = ("classic", 80))
+
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+    
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
